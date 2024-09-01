0c6f2fd8d687192a5c2b704449a33bf3dd3b6049
carlhidalgo <106288856+carlhidalgo@users.noreply.github.com>
2024-09-01 11:51:31 -0400

    se agregaron modulos y comonents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'auth',
     pathMatch: 'full'
   },
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/auth/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  }
+
+  
 ];
 
 @NgModule({
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'] // Cambiado a SCSS
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() title!: string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
