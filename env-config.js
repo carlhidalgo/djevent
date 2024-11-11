5d3bbecf4d33281fba3811644bb7f9027f205859
carlhidalgo <106288856+carlhidalgo@users.noreply.github.com>
2024-11-11 20:58:27 -0300

    se agrego la configuracion para appflow y en local

---
+++ env-config.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+const dotenv = require('dotenv');
+
+
+
+// Detecta si estás en Appflow (ENV debería estar configurado en las variables de entorno de Appflow)
+
+const isAppflow = !!process.env.ENV && !process.env.LOCAL_ENV;
+
+const envFilePath = isAppflow ? null : '.env';
+
+const environmentFile = (process.env.ENV || 'development') === 'development' ? `environment.ts` : "environment.prod.ts";
+
+
+
+// Carga las variables de entorno desde el archivo local si no está en Appflow
+
+let envConfig = {};
+
+if (!isAppflow && envFilePath) {
+
+ envConfig = dotenv.parse(fs.readFileSync(envFilePath));
+
+} else {
+
+ // Usa process.env directamente en Appflow
+
+ envConfig = {
+
+    production: true,
+    API_URL: process.env.API_URL || '',
+    API_KEY_FIREBASE: process.env.API_KEY_FIREBASE || '',
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN || '',
+    DATABASE_URL: process.env.DATABASE_URL || '',
+    PROJECT_ID: process.env.PROJECT_ID || '',
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET || '',
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID || '',
+    APP_ID: process.env.APP_ID || '',
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID || '',
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || ''
+
+ };
+
+}
+
+
+
+// Genera el archivo de entorno para Angular
+
+fs.writeFileSync(
+
+ `./src/environments/${environmentFile}`,
+
+ 'export const environment = ' + JSON.stringify(envConfig) + ';'
+
+);
+
+
+
+console.log(`Archivo de entorno generado en ${environmentFile} para el entorno ${process.env.ENV || 'desarrollo'}`);
+
